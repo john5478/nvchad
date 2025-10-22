79d56f398aa5dcdd99f21cf81d42bb582783714d
john5478 <a10290922@gmail.com>
2025-10-22 17:50:37 +0800

    remove customized config of nvim.cmp

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,13 +23,6 @@ return {
   },
 
   {
-    'hrsh7th/nvim-cmp',
-    opts = function()
-      return require "configs.cmp"
-    end,
-  },
-
-  {
   "debugloop/telescope-undo.nvim",
     dependencies = {
       {
