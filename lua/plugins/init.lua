630e27dda636d8191f83748f888f52d0bf275047
john5478 <a10290922@gmail.com>
2025-10-23 08:44:21 +0800

    Revert "remove customized config of nvim.cmp"
    This reverts commit 79d56f398aa5dcdd99f21cf81d42bb582783714d.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,6 +23,13 @@ return {
   },
 
   {
+    'hrsh7th/nvim-cmp',
+    opts = function()
+      return require "configs.cmp"
+    end,
+  },
+
+  {
   "debugloop/telescope-undo.nvim",
     dependencies = {
       {
