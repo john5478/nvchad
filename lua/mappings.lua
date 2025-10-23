1a8e686818da5eaff1aaf2811b15cc1f8b2026ca
john5478 <a10290922@gmail.com>
2025-10-23 09:52:55 +0800

    add a keybinding to remove trailling spaces

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,5 +10,6 @@ map("n", "<leader>cd", ":cd %:p:h<CR>")
 map('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
 map('i', '<C-j>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
 map('i', '<C-k>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+map('n', '<Leader>ts', [[:%s/\s\+$//e<cr>]])
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
