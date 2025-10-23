4dd0a6c4e3216132eaa9a005e9c333f322524134
john5478 <a10290922@gmail.com>
2025-10-23 09:03:12 +0800

    add a function to check when to apply the clipboard customization

--- init.lua
+++ init.lua
@@ -1,17 +1,40 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
-vim.g.clipboard = {
-  name = 'WslClipboard',
-  copy = {
-    ['+'] = 'clip.exe',
-    ['*'] = 'clip.exe',
-  },
-  paste = {
-    ['+'] = 'pwsh.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ['*'] = 'pwsh.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  },
-  cache_enabled = 0,
-}
+
+-- For WSL clipboard
+local function _is_wsl()
+  local ok, uname = pcall(vim.loop.os_uname)
+  if ok and uname and uname.release then
+    if (
+      string.lower(uname.release):match("microsoft") or
+      string.lower(uname.release):match("wsl")
+    ) then
+      return true
+    end
+  end
+  if vim.fn.has("wsl") == 1 then
+    return true
+  end
+  if vim.env.WSL_DISTRO_NAME ~= nil then
+    return true
+  end
+  return false
+end
+
+if _is_wsl() then
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] = 'pwsh.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'pwsh.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
